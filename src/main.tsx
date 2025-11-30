1e36dc89e03493f640ca62f12a46975ec7928820
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-12-01 04:12:00 +0900

    feat: App.tsx, main.tsx 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import GlobalStyle from './style/GlobalStyle.tsx';
+import { theme } from './style/theme.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
-)
+);
