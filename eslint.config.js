f7824978a34aa3994c4050aab3d9b5fdef05a227
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-12-01 03:09:46 +0900

    setting: 기본 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      react: react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': tsPlugin,
+      'react-refresh': reactRefresh,
+      prettier: prettier,
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      reactRefresh.configs?.vite ?? 'plugin:react-refresh/recommended',
+      prettier,
+    ],
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
   },
-])
+]);
