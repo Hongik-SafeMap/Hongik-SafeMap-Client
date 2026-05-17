7e111f38d11f5900b5bc352aebae19033f763704
kwaktato <109459319+kwaktato@users.noreply.github.com>
2026-05-17 23:36:30 +0900

    fix: 사용자 헤더 알림 아이콘 추가

--- src/components/common/TitleHeader.tsx
+++ src/components/common/TitleHeader.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import Logo from '@/assets/icons/LogoHome.svg?react';
 import My from '@/assets/icons/My.svg?react';
+import Notification from '@/assets/icons/Notification.svg?react';
 import { useHandleNavigate } from '@/hooks/useHandleNavigate';
+import { useNotificationUnread } from '@/api/notification';
 
 interface TitleMainSubProps {
   main: string;
@@ -54,10 +56,27 @@ export const TitleHeader = ({
 }: TitleHeaderProps) => {
   const { handleNavigate } = useHandleNavigate();
 
+  const { data } = useNotificationUnread();
+
   return (
     <TitleHeaderWrapper>
       {home ? <Logo /> : <TitleMainSub main={mainTitle} sub={subTitle} />}
-      <My onClick={() => handleNavigate('/user/my')} />
+
+      <div className="icons">
+        <div
+          className="notification"
+          onClick={() => handleNavigate('/user/notification')}
+        >
+          <Notification />
+          {data > 0 && (
+            <Count isSingleDigit={data > 0 && data < 10}>
+              {data > 99 ? '99+' : data}
+            </Count>
+          )}
+        </div>
+
+        <My onClick={() => handleNavigate('/user/my')} />
+      </div>
     </TitleHeaderWrapper>
   );
 };
@@ -75,6 +94,48 @@ const TitleHeaderWrapper = styled.div`
   top: 0;
   left: 0;
   right: 0;
-
   z-index: 1;
+
+  .icons {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
+
+  .notification {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+`;
+
+const Count = styled.div<{ isSingleDigit: boolean }>`
+  height: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => theme.colors.red600};
+  position: absolute;
+
+  ${({ isSingleDigit }) =>
+    isSingleDigit
+      ? `
+        width: 16px;
+        padding: 0px;
+        border-radius: 50%;
+        top: 0px;
+        right: 0px;
+      `
+      : `
+        padding: 0px 6px;
+        border-radius: 8px;
+        top: 0px;
+        right: -4px;
+      `}
+
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.font.fontSize.detail10};
+  font-weight: ${({ theme }) => theme.font.fontWeight.medium};
 `;
